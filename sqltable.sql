22d9f633753eb2f48980704e9e0515ee2da963ec
fzuycy <2253708742@qq.com>
2021-01-20 12:56:07 +0800

    Create sqltable.sql

---
+++ sqltable.sql
@@ -0,0 +1,7 @@
+CREATE TABLE mytable_one(
+country VARCHAR(32),abbreviation VARCHAR(32),capital_city VARCHAR(32),
+sq_km_area LONG,population LONG,confirmed LONG,recovered LONG,deaths LONG,
+continent VARCHAR(32),location VARCHAR(32),iso VARCHAR(32),elevation_in_meters LONG,life_expectancy DOUBLE);
+
+CREATE TABLE mytable_two(
+LocationName VARCHAR(32),lat DOUBLE,mylong LONG,confirmed LONG,recovered LONG,deaths LONG,updated VARCHAR(32))
